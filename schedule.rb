64e8fe33350c77e2a9fa750d5676f2c8ee739d77
Alexander Morozov <ntcomp12@gmail.com>
2016-04-07 09:12:31 -0400

    ajusted

--- schedule.rb
+++ schedule.rb
@@ -1,6 +1,6 @@
 # send iplirconf
 every 1.hour do
-  wd = "/home/user/vipnet_hw_tools_ruby_dev"
-  ruby = "/usr/bin/ruby"
+  wd = "~/hw_tools"
+  ruby = `which ruby`
   command "cd #{wd}; #{ruby} ./send_all.rb >/dev/null 2>&1"
 end
