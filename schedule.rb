14c0507a91184739b419b4e5c40cb5fb3994d4b6
Alexander Morozov <ntcomp12@gmail.com>
2016-06-22 10:57:06 -0400

    fixed schedule

--- schedule.rb
+++ schedule.rb
@@ -7,11 +7,11 @@ end
 every 1.day, :at => "6am" do
   wd = "~/config"
   rake = `which rake`
-  command "cd #{wd}; rake send_last_n_tickets >/dev/null 2>&1"
+  command "cd #{wd}; #{rake} send_last_n_tickets >/dev/null 2>&1"
 end
 
 every :sunday, :at => "8am" do
   wd = "~/config"
   rake = `which rake`
-  command "cd #{wd}; rake send_all_tickets >/dev/null 2>&1"
+  command "cd #{wd}; #{rake} send_all_tickets >/dev/null 2>&1"
 end
